06b3668058dc16b8a7c5239b6285458cd4b87dc2
gaurav12devloper <gauravprajapati459@gmail.com>
2023-09-06 19:31:31 +0530

    remove dublicate element from vector

---
+++ remove_dublicate.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int size=nums.size();
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                if(nums[i]==nums[j])
+                {
+                    nums.erase(nums.begin()+j);
+                    size--;
+                    j--;
+                }
+            }
+        }
+        return size;
+    }
+};
